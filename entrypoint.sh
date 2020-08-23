8dbbbc80fb30177e1953a4bc491e0e2e1acb213c
plopcas <>
2020-08-23 12:51:16 +0100

    Update permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,7 +40,19 @@ if [ "$DEBUG" = "1" ]; then
   set -x
 fi
 
-# Run "hugo deploy" as configure in your repo
+# Install Hugo
+HUGO_VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | jq -r '.tag_name')
+mkdir tmp/ && cd tmp/
+curl -sSL https://github.com/gohugoio/hugo/releases/download/${HUGO_VERSION}/hugo_extended_${HUGO_VERSION: -6}_Linux-64bit.tar.gz | tar -xvzf-
+mv hugo /usr/local/bin/
+cd .. && rm -rf tmp/
+cd ${GITHUB_WORKSPACE}
+hugo version || exit 1
+
+# Build
+hugo
+
+# Deploy as configured in your repo
 hugo deploy
 
 # Clear out credentials after we're done
